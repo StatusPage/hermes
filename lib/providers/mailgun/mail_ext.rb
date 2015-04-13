9123667cd1895588cc7153baf8528cbc102f8bdd
Scott Klein <scott@klein.io>
2015-04-13 17:36:14 -0400

    some updates to the day domain is named

--- lib/providers/mailgun/mail_ext.rb
+++ lib/providers/mailgun/mail_ext.rb
@@ -1,9 +1,5 @@
 module Mail
   class Message
-    attr_accessor :mailgun_variables
-    attr_accessor :mailgun_options
-    attr_accessor :mailgun_recipient_variables
-    attr_accessor :mailgun_headers
-    attr_accessor :mailgun_domain
+    attr_accessor :domain
   end
 end
