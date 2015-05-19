7d101e84d5e999a18964dd2ae8ed8f63d53388c2
Scott Klein <scott@scottkle.in>
2015-05-18 22:23:11 -0600

    plivo library returns the result code and body in an array

--- lib/providers/plivo/plivo_provider.rb
+++ lib/providers/plivo/plivo_provider.rb
@@ -6,8 +6,8 @@ module Hermes
       payload = payload(rails_message)
 
       if self.deliverer.should_deliver?
-        result = self.client.send_message(payload)
-        rails_message[:message_id] = result["api_id"]
+        code, body = self.client.send_message(payload)
+        rails_message[:message_id] = body["api_id"]
       else
         # rails message still needs a fake sid as if it succeeded
         rails_message[:message_id] = SecureRandom.uuid
